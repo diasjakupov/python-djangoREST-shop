b12851ce939fb18777d985750bddeeaab3642289
diasjakupov <dias.dzhakupov.68@mail.ru>
2020-10-11 20:30:05 +0600

    filter added

--- djangoRest/settings.py
+++ djangoRest/settings.py
@@ -161,7 +161,7 @@ EMAIL_HOST_PASSWORD = 'ktruwkoiefsrebin'
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
